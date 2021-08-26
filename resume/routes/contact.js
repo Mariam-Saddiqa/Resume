7f181bf30d6423f11843fd60b52333a2b7bce497
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-08-26 13:51:30 +0200

    update

--- resume/routes/contact.js
+++ resume/routes/contact.js
@@ -1,10 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'CONTACT' });
-});
+router.get("/", function (req, res, next) {
+  res.render("contact", {
+    title: "CONTACT"
 
+  });
+  
+});
 
 module.exports = router;
