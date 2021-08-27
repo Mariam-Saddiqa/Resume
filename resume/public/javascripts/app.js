b2183674305f32f81f4daada55d182d82d03511d
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-08-27 12:29:28 +0200

    update

--- resume/public/javascripts/app.js
+++ resume/public/javascripts/app.js
@@ -1,1 +1,27 @@
-                    
+const Name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const comment = document.querySelector("#comments");
+const button = document.querySelector(".send");
+
+button.addEventListener("click", addComments);
+
+function addComments(e) {
+  e.preventDefault();
+
+  let data = {
+    name: Name.value,
+    email: email.value,
+    comment: comment.value,
+  };
+
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+    },
+  }).then((res) => {
+    alert("Saved");
+  });
+  console.log(data);
+}
